941d02e8f55b116f43c2f66ff84dfb22e9232e65
Meyvi <meyvim@gmail.com>
2023-12-25 11:10:25 -0800

    Format document

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,40 +1,41 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 interface MovieDetailsData {
-    title: string;
-    overview: string;
-    // popularity: number;
-    // release_data: string;
-    // vote_average: number;
-    // vote_count: number; 
+  title: string;
+  overview: string;
+  // popularity: number;
+  // release_data: string;
+  // vote_average: number;
+  // vote_count: number;
 }
 
 const MovieDetails: React.FC = () => {
-    const { movieId } = useParams<{ movieId: string }>();
-    const [movieDetails, setMovieDetails] = useState<MovieDetailsData | null>(null);
-    const [loading, setLoading] = useState(true);
+  const { movieId } = useParams<{ movieId: string }>();
+  const [movieDetails, setMovieDetails] = useState<MovieDetailsData | null>(
+    null
+  );
+  const [loading, setLoading] = useState(true);
 
-
-useEffect(() => {
+  useEffect(() => {
     const fetchMovieDetails = async () => {
-        try {
-            const apiKey = process.env.REACT_APP_API_KEY;
-            const apiUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`;
+      try {
+        const apiKey = process.env.REACT_APP_API_KEY;
+        const apiUrl = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`;
 
-            const response = await axios.get(apiUrl);
+        const response = await axios.get(apiUrl);
 
-            setMovieDetails(response.data);
-        } catch (error) {
-            console.error('Error fetching movie details:', error);
-        } finally {
-            setLoading(false);
-        }
+        setMovieDetails(response.data);
+      } catch (error) {
+        console.error("Error fetching movie details:", error);
+      } finally {
+        setLoading(false);
+      }
     };
 
     fetchMovieDetails();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [movieId]);
 
   if (loading) {
@@ -47,11 +48,10 @@ useEffect(() => {
 
   return (
     <div>
-        <h2>{movieDetails.title}</h2>
-        <p>{movieDetails.overview}</p>
+      <h2>{movieDetails.title}</h2>
+      <p>{movieDetails.overview}</p>
     </div>
-  )
-  
-}
+  );
+};
 
 export default MovieDetails;
